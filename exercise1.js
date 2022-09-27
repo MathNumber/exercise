40e724f12b41e6afde6cf76b73f0a1e348a9170e
MathNumber <ducpd82@gmail.com>
2022-09-27 18:02:38 +0700

    upload

---
+++ exercise1.js
@@ -0,0 +1,359 @@
+const DATA =  [
+    {
+      "id": "63303bb24ca72639e47660fa",
+      "name": "Tasha Chan",
+      "country": "Andorra",
+      "age": 17,
+      "sex": "male",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb2e9d73494bc220609",
+      "name": "Rebekah Hahn",
+      "country": "Andorra",
+      "age": 25,
+      "sex": "male",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb20d6df0656ded154e",
+      "name": "Frost Mccarthy",
+      "country": "Tonga",
+      "age": 29,
+      "sex": "female",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb25fa0a398f4d9833b",
+      "name": "Rachelle Curry",
+      "country": "Mauritania",
+      "age": 43,
+      "sex": "female",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb21a730f1ad0c267ad",
+      "name": "Avery Roy",
+      "country": "Iceland",
+      "age": 23,
+      "sex": "female",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb23f2391db5c750170",
+      "name": "Felicia Kennedy",
+      "country": "Zambia",
+      "age": 29,
+      "sex": "male",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb24472ae185ff59bb4",
+      "name": "Webb Hanson",
+      "country": "Andorra",
+      "age": 23,
+      "sex": "male",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb2435511b9b375a192",
+      "name": "Lowery Whitley",
+      "country": "Tonga",
+      "age": 17,
+      "sex": "male",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb2c1488b264f93786f",
+      "name": "Maureen Randall",
+      "country": "Guam",
+      "age": 21,
+      "sex": "male",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb272fc16c4fd03a425",
+      "name": "Greer Howe",
+      "country": "Andorra",
+      "age": 29,
+      "sex": "male",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb2505514fab8542900",
+      "name": "Vargas Horton",
+      "country": "Guam",
+      "age": 23,
+      "sex": "male",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb265226242def1b395",
+      "name": "Lena Valenzuela",
+      "country": "Indonesia",
+      "age": 19,
+      "sex": "female",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb28025d932e85cf17d",
+      "name": "Sosa Webb",
+      "country": "Guam",
+      "age": 45,
+      "sex": "male",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb28f661c9b7ddeda70",
+      "name": "Clare Walter",
+      "country": "Mayotte",
+      "age": 35,
+      "sex": "female",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb2776cb29aef1bd132",
+      "name": "Crane Riddle",
+      "country": "Kazakhstan",
+      "age": 21,
+      "sex": "male",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb26fb93eb338dd4f67",
+      "name": "Flynn Sullivan",
+      "country": "Guam",
+      "age": 17,
+      "sex": "male",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb2b3324aaec04bb292",
+      "name": "Deloris Alvarado",
+      "country": "Ireland",
+      "age": 19,
+      "sex": "female",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb20504d9f46fbef0b1",
+      "name": "Geneva Booker",
+      "country": "Mozambique",
+      "age": 15,
+      "sex": "male",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb23b20484d2372a51d",
+      "name": "Enid Cleveland",
+      "country": "Guam",
+      "age": 29,
+      "sex": "male",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb2875221cdce49a585",
+      "name": "Hartman Dotson",
+      "country": "Malawi",
+      "age": 21,
+      "sex": "female",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb2e48e2f57ab39be27",
+      "name": "Tammy Kirkland",
+      "country": "Andorra",
+      "age": 77,
+      "sex": "female",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb2abff9451155cd124",
+      "name": "Lucinda Garrison",
+      "country": "Tonga",
+      "age": 29,
+      "sex": "male",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb2a69315e71c8fae31",
+      "name": "Sanchez Harris",
+      "country": "Tonga",
+      "age": 53,
+      "sex": "male",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb2f1b7180404a33ab6",
+      "name": "Minnie Bullock",
+      "country": "Yugoslavia",
+      "age": 21,
+      "sex": "male",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb2d47d02728dc0db89",
+      "name": "Walters Madden",
+      "country": "Andorra",
+      "age": 57,
+      "sex": "female",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb2ab8cbf65ecf0f7a6",
+      "name": "Juarez Drake",
+      "country": "Uruguay",
+      "age": 21,
+      "sex": "female",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb20a7c0cd8eccc8d5a",
+      "name": "Carmella Waters",
+      "country": "Andorra",
+      "age": 29,
+      "sex": "female",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb29bced809c97a2720",
+      "name": "Christina Gray",
+      "country": "Andorra",
+      "age": 21,
+      "sex": "male",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb2d46781b7ffdddac9",
+      "name": "Ginger Wilkerson",
+      "country": "Gabon",
+      "age": 37,
+      "sex": "female",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb200edb4ce35c5f327",
+      "name": "Rhonda Chen",
+      "country": "Poland",
+      "age": 55,
+      "sex": "male",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb2e85b0eb6f9cab315",
+      "name": "Lakisha Bowen",
+      "country": "Thailand",
+      "age": 21,
+      "sex": "female",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb2a762a877dbbbebb1",
+      "name": "Nixon Ferguson",
+      "country": "Tonga",
+      "age": 23,
+      "sex": "female",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb28a7683cf779ade80",
+      "name": "Ingram Buckner",
+      "country": "Madagascar",
+      "age": 13,
+      "sex": "female",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb2e011af1db3a2af19",
+      "name": "Mcmahon Ward",
+      "country": "Tonga",
+      "age": 21,
+      "sex": "female",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb22533351af07dceb2",
+      "name": "Gwen Benjamin",
+      "country": "Iran",
+      "age": 17,
+      "sex": "female",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb23427eb4aeb711661",
+      "name": "Weiss Kaufman",
+      "country": "Tonga",
+      "age": 13,
+      "sex": "male",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb2af2e3f44696ac8f2",
+      "name": "Laura Crane",
+      "country": "American Samoa",
+      "age": 29,
+      "sex": "female",
+      "isInfected": false
+    },
+    {
+      "id": "63303bb223fbd679f7ed2c4a",
+      "name": "Chrystal Bonner",
+      "country": "Tonga",
+      "age": 27,
+      "sex": "male",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb25f7d176f602b1a2a",
+      "name": "Sharpe Reese",
+      "country": "Tonga",
+      "age": 25,
+      "sex": "female",
+      "isInfected": true
+    },
+    {
+      "id": "63303bb2c6e945d454603cff",
+      "name": "Harper Jennings",
+      "country": "Martinique",
+      "age": 23,
+      "sex": "male",
+      "isInfected": false
+    },
+];
+
+// Cho mảng DATA là danh sách các công dân tham gia khảo sát COVID 19
+// Thực hiện các yêu cầu sau:
+// 1. Liệt kê danh sách các công dân đến từ Andorra
+// 2. Có bao nhiêu công dân đã từng bị nhiễm COVID? Liệt kê danh sách các công dân đã từng nhiễm COVID 19 là phụ nữ dưới 20 tuổi
+// 3. Sắp xếp và liệt kê danh sách các công dân đến từ Tonga giảm dần theo tuổi
+// 4. Có tất cả bao nhiêu công dân trong độ tuổi từ 30 đến 60 chưa từng nhiễm COVID 19?
+// 5. Thêm thuộc tính isSurvey: true cho các công dân trong danh sách
+    for (var key of DATA){
+      if (key.country =='Andorra'){
+        console.log(`Cong dan den tu Andorra la: ${key.name}`);
+      }
+    }
+    for (var key of DATA){
+      if (key.isInfected==true){
+        console.log(`So nguoi nhiem covid la: ${key.name}`);
+      }
+    
+      if (key.isInfected==true&&key.sex=='female'&&key.age<20){
+          console.log(` Nhung nguoi bi nhiem covid la phu nu duoi 20 tuoi la ${key.name}`)
+      }
+    }
+
+    for (var key of DATA){
+      if (key.age>=30&&key.age<=60){
+          if (key.isInfected==false){
+            console.log(`cong dan trong do tuoi tu 30 den 60 chua bi nhiem covid la: ${key.name}`);
+          }
+      }
+    }
+    for(var key of DATA){
+      if (key.country=='Tonga'){
+          console.log(`Danh sach cong dan den tu Tonga la: ${key.name} `);
+      }
+    }
+    var newArr=[...DATA,'isSurvey:true'];
+    console.log(newArr);
